d60000ff627c0e1973b8575deeda2d2cc8864283
edu <britoduk@gmail.com>
2026-03-26 16:43:45 -0300

    adição de imagens bordas, e qr code

--- app/src/main/java/com/senai/carteirinhadigital/MainActivity.kt
+++ app/src/main/java/com/senai/carteirinhadigital/MainActivity.kt
@@ -5,19 +5,31 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.rafaelcosta.carteirinhadigital_4devm_t1.feature.carteirinha.presentation.component.QrCode
 import com.senai.carteirinhadigital.ui.theme.CarteirinhaDigitalTheme
+import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,28 +37,85 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CarteirinhaDigitalTheme {
-                Scaffold{
-                    innerPadding ->
-                    CarteirinhaDigitalApp(modifier = Modifier
-                        .padding(paddingValues = innerPadding)
-                        .fillMaxSize()
+                Scaffold { innerPadding ->
+                    CarteirinhaDigitalApp(
+                        modifier = Modifier
+                            .padding(paddingValues = innerPadding)
+                            .fillMaxSize()
                     )
                 }
             }
         }
     }
 }
+
 @Composable
 fun CarteirinhaDigitalApp(modifier: Modifier = Modifier) {
+    Box{
+        Image(
+            painter = painterResource(R.drawable),
+            modifier = Modifier
+                .fillMaxSize()
+        )
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(100.dp)
-    ) {Image(
-        painter = painterResource(R.drawable.senai),
-        contentDescription = null)
+        verticalArrangement = Arrangement.spacedBy(30.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.senai),
+            contentDescription = null
+        )
 
         Image(
             painter = painterResource(R.drawable.avatar),
-            contentDescription = null)}
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(300.dp)
+                .clip(CircleShape)
+                .border(2.dp, Color.Black, CircleShape)
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            Text(
+                text = "Nome:",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+            )
+            Text(text = "Eduardo Brito",
+                fontSize = 18.sp,)
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            Text(
+                text = "Curso:",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+            )
+            Text(text = "Desenvolvimento de Sistemas",
+                fontSize = 18.sp,)
+        }
+        QrCode(
+            "90000000001755046730",
+            modifier = Modifier
+                .border(
+                    4.dp,
+                    Color.Black
+                )
+        )
+    }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun CarteirinhaDigitalAppPreview() {
+    CarteirinhaDigitalTheme {
+        CarteirinhaDigitalApp()
+    }
 }
